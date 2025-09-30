59ad3d333e496ad22d2af5cea538e4be550de875
Gabriel amaral <amaralgabriel357@gmail.com>
2025-09-30 12:35:54 -0300

    comecei a fazer o código

---
+++ CosnsultaCep/ConsultaCep.java
@@ -0,0 +1,21 @@
+package CosnsultaCep;
+
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultaCep {
+
+    public Endereco buscaEndereco(String cep){
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://viacep.com.br/ws/" + cep + "/json/"))
+                .build();
+        HttpResponse<String> response = null;
+
+        return new Gson().fromJson(response.body(),Endereco.class);
+    }
+}
---
+++ CosnsultaCep/Main.java
@@ -0,0 +1,13 @@
+package CosnsultaCep;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    //Scanner sc = new Scanner(System.in);
+    //String cep = sc.nextLine();
+
+        ConsultaCep consultaCep = new ConsultaCep();
+        ConsultaCep.busca
+    }
+}
